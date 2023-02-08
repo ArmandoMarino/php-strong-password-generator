240bf3ad571db1f4d585b2dd9ab1ebc93d62bd43
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-08 15:52:17 +0100

    idea of possibles arrays password

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 $user_length = $_GET['password-length'] ?? '';
 var_dump($user_length);
 
-
-function randomPassword($number) {
-    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890%&$£!0123456789';
+// Here i pass only the value i want to manipulate : $number (generic) and i pass later $user_length
+function randomPassword($user_number) {
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $numbers = '1234567890';
+    $symbols = '£$%&?!';
     $pass = array(); //remember to declare $pass as an array
     $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-    for ($i = 0; $i < $number; $i++) {
+    for ($i = 0; $i < $user_number; $i++) {
         $n = rand(0, $alphaLength);
         $pass[] = $alphabet[$n];
     }
