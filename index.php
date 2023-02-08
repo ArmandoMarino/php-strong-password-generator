dbaa70881722392dd7ed733c745ca8fdfe12f84b
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-08 15:46:24 +0100

    function random password generator

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 $user_length = $_GET['password-length'] ?? '';
 var_dump($user_length);
 
+
+function randomPassword($number) {
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890%&$£!0123456789';
+    $pass = array(); //remember to declare $pass as an array
+    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+    for ($i = 0; $i < $number; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass); //turn the array into a string
+}
+
+echo randomPassword($user_length);
 ?>
 
 
